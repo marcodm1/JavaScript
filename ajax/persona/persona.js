f93b5159753c80902e8f8af8945b7090a7e5f4b1
Marco Dominguez <marcodomat@gmail.com>
2021-02-17 22:22:37 +0100

    123

--- ajax/persona/persona.js
+++ ajax/persona/persona.js
@@ -9,12 +9,13 @@ function cargarEventos(evento) {
         var peticion = new XMLHttpRequest();
         peticion.onreadystatechange = function() { // cuando la  peticion. el estado este todo ok
             if (this.readyState == 4 && this.status == 200) { // si .readyState este en 4, que es finalizado, y status = 200 que es finalizado
-                var miobjeto = JSON.parse(this.responseText);
+                var miobjeto = JSON.parse(this.responseText); // parse convierte, como parseInt, y en este caso l oconvierte a Json responseText es lo que devuelva el servidor
                 document.getElementById("contenedor1").innerHTML = miobjeto.nombre; // haz esto: responsetext es lo que devuelve el servidor
             }
         }
-        var miPersona = crearPersona();
-        peticion.open("GET", "http://localhost/JavaScript/ajax/persona/persona.php?x=" + JSON.stringify(miPersona), true);
+        var miPersona  = crearPersona();
+        var objetoJson = JSON.stringify(miPersona); 
+        peticion.open("GET", "http://localhost/JavaScript/ajax/persona/persona.php?" + objetoJson, true);
         peticion.send(null);
     }
 
@@ -29,7 +30,7 @@ function crearPersona() {
             this.edad   = edad;
         }
     }
-    var nombre  = document.getElementById("strNombre").value;
+    var nombre  = document.getElementById("strNombre").value; // devuevle un objeto del id
     var edad    = document.getElementById("intEdad").value;
     var persona = new Persona(nombre, edad);
 
--- ajax/persona/persona.php
+++ ajax/persona/persona.php
@@ -1,27 +1,19 @@
 <?php
-    // crear un objeto con lo que le quiero enviar
-    class Persona {
-        public $nombre;
-        public $edad;
-
-        public function __constructor() {
-
-        }
-    }
-  
-
-    $persona1 = new Persona();
-    $persona2 = new Persona();
-
-
-    if (!isset($_GET['x']) ){
-        echo "No se ha recibido el get de x";
+    if (!isset($_GET['nombre']) ) {
+        echo "no existe";
     }else {
-        if ($persona1.edad >= 18) {
+        $objetoJson = json_decode($_GET['valoresObjeto']);
 
-        }
+        var_dump($objetoJson);
+
+        // if (!isset($_GET['valoresObjeto']) ){
+        //     echo "No se ha recibido el get de x";
+        // }else {
+        //     if ($_GET['valoresObjeto'] >= 18) {
+
+        //     }
+        // }
     }
     
-
 
 ?>
