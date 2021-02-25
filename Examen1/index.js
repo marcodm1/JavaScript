780bb8e078bc304d21d760c648c473440407cf59
Marco Dominguez <marcodomat@gmail.com>
2021-02-25 19:56:13 +0100

    123

--- Examen1/index.js
+++ Examen1/index.js
@@ -1,4 +1,4 @@
-
+// meterlo en un html?
 if (localStorage.getItem("Ultima conexion") != null) {
     var ff               = localStorage.getItem("Ultima conexion");
     var ultimaConexion   = Date.parse(ff);
@@ -10,46 +10,109 @@ if (localStorage.getItem("Ultima conexion") != null) {
     var dias             = Math.round(horas/24);
     var texto = `Hacia ${dias} dias, ${horas} horas y ${minutos} minutos que no entrabas.`;
     modificarContenido(texto);
-    var ultimaConexion = "Ultima conexion";
-    var hora           = new Date();
-    localStorage.setItem(ultimaConexion, hora);
-
-    // posicion: 
-    
-    navigator.geolocation.getCurrentPosition(
-        (posicion)=>{
-            var texto = "La latitud = ";
-            añadirContenido(texto, posicion.coords.latitude);}, 
-        (error)=>{ alert("Se ha producido el siguiente error" + error.message); }
-    );
-   
-    navigator.geolocation.getCurrentPosition(
-        (posicion)=>{
-            var texto = "La longitud = ";
-            añadirContenido(texto, posicion.coords.longitude);}, 
-        (error)=>{ alert("Se ha producido el siguiente error" + error.message); }
-    );
-
+    localStorage.setItem("Ultima conexion", new Date());
+    conseguirCoordenadas();
 }else {
-    var hora = new Date();
-    localStorage.setItem("Ultima conexion", hora);
-    var texto = "Es visita esta página.";
+    localStorage.setItem("Ultima conexion", new Date());
+    var texto = "Es la primera vez que visita esta página.";
     modificarContenido(texto);
+    conseguirCoordenadas();
 }
-
 
 
 function modificarContenido(texto) { 
     document.getElementById("ultima_visita").innerHTML = texto;
 }
 
+function rellenarTabla(json) { 
+    document.getElementById("ultima_visita").innerHTML = json.edad;
+}
+
+
 function añadirContenido(texto, contenido) { 
     var hijo       = document.createElement("p");
     hijo.innerHTML = texto + contenido; 
     var padre      = document.getElementsByTagName("hr")[0];
     padre.appendChild(hijo);
-  
+}
+
+function conseguirCoordenadas() {
+    conseguirLatitud();
+    conseguirLongitud();
+}
+
+function conseguirLatitud() {
+    navigator.geolocation.getCurrentPosition(
+        (posicion)=>{ // es una funcion
+            var texto = "La latitud = ";
+            añadirContenido(texto, posicion.coords.latitude);
+            localStorage.setItem("Latitud", posicion.coords.latitude);
+        }, 
+        (error)=>{
+            alert("sdfsd");
+            if (localStorage.getItem("Latitud") != null) {
+                añadirContenido("La latitud = ", localStorage.getItem("Latitud"));
+            } else {
+                añadirContenido("La latitud = ", posicion.coords.latitude);
+                localStorage.setItem("Latitud", posicion.coords.latitude);
+            }
+        },
+        { 
+            timeout:3000
+        } 
+    );
+}
+
+function conseguirLongitud() {
+    navigator.geolocation.getCurrentPosition(
+        (posicion)=>{
+            añadirContenido("La longitud = ", posicion.coords.Longitud);
+            localStorage.setItem("Longitud", posicion.coords.Longitud);
+        }, 
+        (error)=>{
+            if (localStorage.getItem("Longitud") != null) {
+                añadirContenido("La Longitud = ", localStorage.getItem("Longitud"));
+            } else {
+                añadirContenido("La Longitud = ", posicion.coords.Longitud);
+                localStorage.setItem("Longitud", posicion.coords.Longitud);
+            }
+        },
+        { 
+            timeout:3000
+        } 
+    );
+}
+
+document.addEventListener("readystatechange", cargarEventos, false);
+
+function cargarEventos(evento) {
+    if (document.readyState == "interactive") {
+        document.getElementById("boton").addEventListener("click", leerRegistro, true);
+    }
+
+    function leerRegistro() {
+        var campo = document.getElementById("edad").value;
+        if (campo > 0 || campo < 20) {
+            enviarRecibir();
+        }else {
+            alert("El dato introducido no es correcto");
+        }
+    }
 }
 
 
-
+function enviarRecibir(evento) {
+    evento.preventDefault(); // hace que no se comporte como por defecto, para que no haga submit del formulario
+    var peticion = new XMLHttpRequest();
+    
+    peticion.onreadystatechange = function() { // cuando la  peticion. el estado este todo ok
+        if (this.readyState == 4 && this.status == 200) { // si .readyState este en 4, que es finalizado, y status = 200 que es finalizado
+            var miobjeto = JSON.parse(this.responseText); // parse convierte, como parseInt, y en este caso l oconvierte a Json responseText es lo que devuelva php
+            rellenarTabla(miobjeto); // haz esto: responsetext es lo que devuelve el servidor
+        }
+    }
+    var edad = document.getElementById("edad").value;
+    var objJson     = JSON.stringify(edad);
+    peticion.open("POST", "http://localhost/JavaScript/Examen1/leer.php", true);
+    peticion.send(objJson); // el parametro es el body
+}
--- Examen1/leer.php
+++ Examen1/leer.php
@@ -1,8 +1,27 @@
 <?php
 	
-	$movimientos = array  (
-		array("2019-01-15", "Apertura de cuenta", 1200),
+	// $movimientos = array  (
+	// 	array("2019-01-15", "Apertura de cuenta", 1200),
 		
-		array("2019-01-17", "Ingreso en efectivo", 100));
+	// 	array("2019-01-17", "Ingreso en efectivo", 100)
+	// );
 	
+	class Persona {
+        public $edad;
+        public function __construct($edad){
+            $this->edad = "Te llamas " . $edad;
+        }
+    }
+    $edad = file_get_contents('php://input'); // para recoger el body, o lo que le envia con "peticion.send(objJson);"
+
+    $edad = json_decode($edad); // lo convierte en objeto php
+
+    $edad = new Persona($edad); // lo convierto de objeto a objeto Persona
+    
+    $edad = json_encode($edad);
+
+    echo $edad;
+
+
+
 ?>
